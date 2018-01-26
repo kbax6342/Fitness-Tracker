7c4d80483eda50e94972d92c1995f89a9ac5f72c
Kevin Baxter <k3vin.baxt3r@gmail.com>
2018-01-25 19:00:34 -0500

    made seperate chest workout class that functions with User class and the main class

--- src/Business/ChestWorkOut.java
+++ src/Business/ChestWorkOut.java
@@ -2,10 +2,78 @@ package Business;
 
 import java.util.Scanner;
 
-public class ChestWorkOut extends Logic{
+public class ChestWorkOut { 
+	
+
+	public static String chestWorkout;
+	public static Scanner chestAnswer = new Scanner(System.in);
+	public static Scanner answer = new Scanner(System.in);
+	public static double chestWorkoutNumber;
+	public static double newWeight;
+	public static String continueAnswer;
 	
 	
+	public static double updatedWeight;
+	public static double startingWeight;
+	public static double differenceWeight;
+	
+
+	public static void chestWelcomeMessage(User user1) {
+		
+		//Shows the result of the choice
+		System.out.println("You chose the chest workout today!!");
+		
+	
+		//Will eventually change the choice options to a string
+		System.out.println("Please pick a workout out:\n1.Bench Press \n2.Incline Press \n3. Decline Press");
+		chestWorkout = chestAnswer.nextLine();
+		
+		if(chestWorkout.equalsIgnoreCase("bench press")) {
+			
+			//Getting current weight from the user
+			System.out.println("Your current weight is: "+  user1.getWeight() + "\n Would you like to update your weight: yes or no");
+			String updateAnswer = answer.nextLine();
+			
+			if(updateAnswer.equalsIgnoreCase("yes")) {
+				
+				System.out.println("Please enter your new weight");
+				newWeight = answer.nextInt();
+				
+				user1.setWeight(newWeight);
+				
+			}
+			
+			
+			System.out.println("Enter Bench Press Weight:");
+			chestWorkoutNumber = chestAnswer.nextDouble();
+			
+	
+			//I will save the starting weight number always so they don't have to remember to enter weight every time
+			System.out.println("You entered " +  chestWorkoutNumber + " !");
+			
+			System.out.println("Great Job! \n Did you want to add more Weight: yes or no ");
+			answer.nextLine();
+			continueAnswer = answer.nextLine();
+			
+			if(continueAnswer.equalsIgnoreCase("yes")) {
+				System.out.println("Please enter weight: ");
+				updatedWeight = answer.nextDouble();
+				startingWeight = user1.getWeight();
+				if(updatedWeight > startingWeight) {
+					differenceWeight = updatedWeight - startingWeight;
+					//find api based on geolocation that can tell what metric system is used in that location
+					System.out.println("You went up " + differenceWeight + " pounds!");
+				}else if(updatedWeight < startingWeight) {
+					differenceWeight = updatedWeight = startingWeight;
+					System.out.println("You went down " + differenceWeight + " pounds!\n" + "Don't give up!");
+					
+				}
+			}
+	}
+		
+		
 	
 	
 
+}
 }
--- src/Business/Logic.java
+++ src/Business/Logic.java
@@ -65,58 +65,11 @@ public class Logic {
 		
 		if(workoutChoice.equalsIgnoreCase("chest")) {
 			
+				ChestWorkOut.chestWelcomeMessage(user1);
 			
-			//Shows the result of the choice
-			System.out.println("You chose the chest workout today!!");
 			
-			//Giving the User the option to choose which workout they are doing that day
 			
-			//Will eventually change the choice options to a string
-			System.out.println("Please pick a workout out:\n 1.Bench Press \n 2.Incline Press \n3. Decline Press");
 			
-			chestWorkout = chestAnswer.nextLine();
-			if(chestWorkout.equalsIgnoreCase("bench press")) {
-				
-				//Getting current weight from the user
-				System.out.println("Your current weight is: "+ user1.getWeight() + "\n Would you like to update your weight: yes or no");
-				String updateAnswer = answer.nextLine();
-				
-				if(updateAnswer.equalsIgnoreCase("yes")) {
-					
-					System.out.println("Please enter your new weight");
-					newWeight = answer.nextInt();
-					
-					user1.setWeight(newWeight);
-					
-				}
-				
-				
-				System.out.println("Enter Bench Press Weight:");
-				chestWorkoutNumber = chestAnswer.nextInt();
-				
-		
-				//I will save the starting weight number always so they don't have to remember to enter weight every time
-				System.out.println("You entered " +  chestWorkoutNumber + " !");
-				
-				System.out.println("Great Job! \n Did you want to add more Weight: yes or no ");
-				answer.nextLine();
-				continueAnswer = answer.nextLine();
-				
-				if(continueAnswer.equalsIgnoreCase("yes")) {
-					System.out.println("Please enter weight: ");
-					updatedWeight = answer.nextDouble();
-					startingWeight = user1.getWeight();
-					if(updatedWeight > startingWeight) {
-						differenceWeight = updatedWeight - startingWeight;
-						//find api based on geolocation that can tell what metric system is used in that location
-						System.out.println("You went up " + differenceWeight + " pounds!");
-					}else if(updatedWeight < startingWeight) {
-						differenceWeight = updatedWeight = startingWeight;
-						System.out.println("You went down " + differenceWeight + " pounds!\n" + "Don't give up!");
-						
-					}
-				}
-			}
 			//Output Chest workout
 		}if(workoutChoice.equalsIgnoreCase("back")) {
 			System.out.println("You chose the back workout");
